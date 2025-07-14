ae64938118740a5dd9737a008f4797ceeacc21e4
minsDreamFood <minsDreamFood@gmail.com>
2025-07-14 15:53:42 +0800

    Add favicons and web app icons in nuxt.config.ts and public directory for improved branding and user experience

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,17 @@ export default defineNuxtConfig({
       link: [
         { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300;400;500;700&display=swap' },
-        { rel: 'stylesheet', href: 'https://unpkg.com/aos@2.3.4/dist/aos.css' }
+        { rel: 'stylesheet', href: 'https://unpkg.com/aos@2.3.4/dist/aos.css' },
+        // Favicons and web app icons
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/android-chrome-192x192.png' },
+        { rel: 'icon', type: 'image/png', sizes: '512x512', href: '/android-chrome-512x512.png' },
+        { rel: 'mask-icon', color: '#5bbad5', href: '/safari-pinned-tab.svg' },
+        { rel: 'msapplication-TileImage', content: '/mstile-150x150.png' }
       ],
       script: [
         { src: 'https://unpkg.com/aos@2.3.4/dist/aos.js' }
